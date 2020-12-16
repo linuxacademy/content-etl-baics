2425b698b2e243aa71dc2f6f12cf62ca90109a83
david thomas <david.thomas@linuxacademy.com>
2020-12-16 13:35:58 -0500

    SQL for contacts table

---
+++ sample-data/contacts.sql
@@ -0,0 +1,5 @@
+CREATE TABLE contacts ( ID int, Name text, Email text);
+INSERT INTO contacts (ID, Name, Email) values ('0', 'David', 'david.thomas@acloud.guru');
+INSERT INTO contacts (ID, Name, Email) values ('1', 'Clay', 'clay@email');
+INSERT INTO contacts (ID, Name, Email) values ('2', 'Sue', 'sue@email');
+INSERT INTO contacts (ID, Name, Email) values ('3', 'Betty', 'betty@email');
