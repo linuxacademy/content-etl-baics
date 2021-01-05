34127ad2a90d2edfbaded5c0269896ad8d6dba95
david thomas <david.thomas@linuxacademy.com>
2021-01-05 10:40:27 -0500

    sample data for transformation lab

---
+++ sample-data/expenses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE expenses ( Code int, Amount money, Description text);
+INSERT INTO expenses (Code, Amount, Description) valuess ('100', '25.50', 'Lunch with clients');
+INSERT INTO expenses (Code, Amount, Description) values ('300', '55.00', 'Dinner with clients');
+INSERT INTO expenses (Code, Amount, Description) values ('200', '75.00', 'Team Lunch');
+INSERT INTO expenses (Code, Amount, Description) values ('400', '150.00', 'air fair');
+INSERT INTO expenses (Code, Amount, Description) values ('100', '20.50', 'Lunch');
+INSERT INTO expenses (Code, Amount, Description) values ('200', '15.75', 'taxi');
+INSERT INTO expenses (Code, Amount, Description) values ('400', '35.75', 'Lunch with clients');
+INSERT INTO expenses (Code, Amount, Description) values ('100', '65.50', 'Lunch with clients');
